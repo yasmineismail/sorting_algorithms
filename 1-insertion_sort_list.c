33184b3c7737eae792032555a78d5139612ae28d
yasmineismail <yasmineiismail@gmail.com>
2023-10-24 10:42:48 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,19 +13,16 @@ void insertion_sort_list(listint_t **list)
 	tp = *list;
 	while (tp)
 	{
-		while (tp)
+		while (tp && tp->next)
 		{
-			if (tp->next)
+			if (tp->i > tp->next->i)
 			{
-				if (tp->i > tp->next->i)
-				{
-					i = tp->i;
-					*(int *)&tp->i = tp->next->i;
-					*(int *)&tp->next->i = i;
-					tp = *list;
-					print_list(*list);
-					break;
-				}
+				i = tp->i;
+				*(int *)&tp->i = tp->next->i;
+				*(int *)&tp->next->i = i;
+				tp = *list;
+				print_list(*list);
+				break;
 			}
 			tp = tp->next;
 		}

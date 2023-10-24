f9a1525c7a74f3fbc26c9b0acb772cd63078cd3c
yasmineismail <yasmineiismail@gmail.com>
2023-10-24 10:36:05 -0700

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+* insertion_sort_list - Function sort array with insertion sort
+* @list: The linked list that will sort
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tp;
+	int i;
+
+	if (!list)
+		return;
+	tp = *list;
+	while (tp)
+	{
+		while (tp)
+		{
+			if (tp->next)
+			{
+				if (tp->i > tp->next->i)
+				{
+					i = tp->i;
+					*(int *)&tp->i = tp->next->i;
+					*(int *)&tp->next->i = i;
+					tp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tp = tp->next;
+		}
+	}
+}
